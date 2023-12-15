eb0b5feeac1aecb1f806a62d3ef7ff152666925f
Gregory Casamento <greg.casamento@gmail.com>
2023-12-15 07:30:58 -0500

    Initial Commit

---
+++ PodsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PodsTest
+//
+//  Created by Gregory John Casamento on 12/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
